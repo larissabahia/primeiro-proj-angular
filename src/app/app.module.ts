9f42ee1f2c883e00ba789412091a669cd0576426
larissa-bahia <larissaj.b@hotmail.com>
2022-11-01 14:34:18 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-@NgModule({
+
+@NgModule({ //declaration serve p registrar componentes, pipes, diretivas e outras estruturas
+
   declarations: [
-    AppComponent
+    AppComponent,
+    ProdutoComponent,
+    NavbarComponent
   ],
-  imports: [
+  imports: [ // imports serve para registrar outros modulos dentro de outro modulo
+  //serve exclusivamente p outros modulos
     BrowserModule
   ],
-  providers: [],
+  providers: [],//providers serve p registrar serviços e interceptadores http
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/produto/produto.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'card-produto',
+    templateUrl: './produto.component.html',
+    styleUrls:['./produto.component.css']
+})
+export class ProdutoComponent {
+
+}
